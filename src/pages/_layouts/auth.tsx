7cb518bc716d41bdb3af3d21b21ffb876068853b
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-01-26 02:16:39 -0300

    add background.png and logo.svg to AuthLayout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,9 +3,19 @@ import { Outlet } from 'react-router'
 export function AuthLayout() {
   return (
     <div className="flex min-h-screen antialiased">
-      <div className="flex h-full w-[755px] min-w-0 flex-1 flex-col">
-        <div>Logo do Marketplace</div>
-        <div>Imagem do Marketplace</div>
+      <div className="flex h-full w-[755px] min-w-[347px] flex-1 flex-col gap-[51px]">
+        <div className="flex gap-5 px-10 pt-10">
+          <img src="/logo.svg" alt="Logo" className="h-[69px] w-[90px]" />
+          <div className="flex flex-col justify-center gap-1">
+            <span className="font-title-md">Marketplace</span>
+            <span className="font-body-md">Painel do Vendedor</span>
+          </div>
+        </div>
+        <img
+          src="/background.png"
+          alt="Background"
+          className="max-h-[496px] max-w-[755px] object-contain"
+        />
       </div>
       <div className="min-w-[611px] flex-shrink-0">
         <Outlet />
