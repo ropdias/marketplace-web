d9b0733b1c008aa0e65ceb8aeb8b1e3ce90ca272
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-01-26 19:53:38 -0300

    update auth layout to adjust based on screen size

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 export function AuthLayout() {
   return (
     <div className="flex min-h-screen antialiased">
-      <div className="flex h-full w-[755px] min-w-[347px] flex-1 flex-col gap-[51px]">
+      <div className="flex h-full w-[755px] min-w-[347px] flex-1 flex-col justify-between gap-4">
         <div className="flex gap-5 px-10 pt-10">
           <img src="/logo.svg" alt="Logo" className="h-[69px] w-[90px]" />
           <div className="flex flex-col justify-center gap-1">
@@ -14,10 +14,10 @@ export function AuthLayout() {
         <img
           src="/background.png"
           alt="Background"
-          className="max-h-[496px] max-w-[755px] object-contain"
+          className="object-contain"
         />
       </div>
-      <div className="min-w-[611px] flex-shrink-0">
+      <div className="h-screen min-w-[611px] flex-shrink-0 p-6">
         <Outlet />
       </div>
     </div>
