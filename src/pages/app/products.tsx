9e800185954df8ca7042d5c4454a293b3b84bec8
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-02-09 20:38:48 -0300

    products page: initial design based on figma

--- src/pages/app/products.tsx
+++ src/pages/app/products.tsx
@@ -1,10 +1,61 @@
+import { Search01Icon } from 'hugeicons-react'
 import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { getTailwindClass } from '@/lib/tailwindUtils'
+import { cn } from '@/lib/utils'
 
 export function Products() {
   return (
-    <>
+    <div className="m-auto flex w-full max-w-[66.875rem] flex-1 flex-col gap-10 px-5">
       <Helmet title="Produtos" />
-      <h1>Produtos</h1>
-    </>
+      <div className="flex flex-col gap-2">
+        <p className={cn('text-gray-500', getTailwindClass('font-title-md'))}>
+          Seus Produtos
+        </p>
+        <p className={cn('text-gray-300', getTailwindClass('font-body-sm'))}>
+          Acesse gerencie a sua lista de produtos à venda
+        </p>
+      </div>
+      <div className="flex-start flex gap-6">
+        <div className="flex w-[20.4375rem] flex-none flex-col gap-6 rounded-[20px] bg-white p-6">
+          <p className={cn('text-gray-300', getTailwindClass('font-title-sm'))}>
+            Filtrar
+          </p>
+          <div className="flex w-full flex-col gap-10">
+            <div className="flex flex-col gap-5">
+              <Input
+                id="search"
+                isFilled={false}
+                placeholder="Pesquisar"
+                iconBefore={Search01Icon}
+              ></Input>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Status" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="announced">Anunciado</SelectItem>
+                  <SelectItem value="sold">Vendido</SelectItem>
+                  <SelectItem value="cancelled">Cancelado</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <Button size="medium" className="justify-center">
+              Aplicar Filtro
+            </Button>
+          </div>
+        </div>
+        <div className="w-full"></div>
+      </div>
+    </div>
   )
 }
