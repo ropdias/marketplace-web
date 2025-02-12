7859e7c3e150ffdb16e12a1e92748763024d3118
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-02-12 01:33:56 -0300

    add product items to products page with example pictures

--- src/pages/app/products.tsx
+++ src/pages/app/products.tsx
@@ -2,6 +2,7 @@ import { SaleTag02Icon, Search01Icon } from 'hugeicons-react'
 import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 
+import { ProductItem } from '@/components/product-item'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import {
@@ -16,20 +17,15 @@ import { cn } from '@/lib/utils'
 
 export function Products() {
   const [selectedValue, setSelectedValue] = useState<string>('')
-  const [selectedValue2, setSelectedValue2] = useState<string>('')
 
   const clearSelection = () => {
     setSelectedValue('')
   }
 
-  const clearSelection2 = () => {
-    setSelectedValue2('')
-  }
-
   console.log(selectedValue)
 
   return (
-    <div className="m-auto flex w-full max-w-[66.875rem] flex-1 flex-col gap-10 px-5">
+    <div className="m-auto flex w-full max-w-[66.875rem] flex-1 flex-col gap-10 px-5 pb-5">
       <Helmet title="Produtos" />
       <div className="flex flex-col gap-2">
         <p className={cn('text-gray-500', getTailwindClass('font-title-md'))}>
@@ -40,7 +36,7 @@ export function Products() {
         </p>
       </div>
       <div className="flex-start flex gap-6">
-        <div className="flex w-[20.4375rem] flex-none flex-col gap-6 rounded-[20px] bg-white p-6">
+        <div className="flex h-full w-[20.4375rem] flex-none flex-col gap-6 rounded-[20px] bg-white p-6">
           <p className={cn('text-gray-300', getTailwindClass('font-title-sm'))}>
             Filtrar
           </p>
@@ -66,27 +62,62 @@ export function Products() {
                   <SelectItem value="cancelled">Cancelado</SelectItem>
                 </SelectContent>
               </Select>
-              <Select value={selectedValue2} onValueChange={setSelectedValue2}>
-                <SelectTrigger
-                  labelText="TESTE"
-                  onClear={clearSelection2}
-                  selectedValue={selectedValue2}
-                >
-                  <SelectValue placeholder="Status" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="announced">Anunciado</SelectItem>
-                  <SelectItem value="sold">Vendido</SelectItem>
-                  <SelectItem value="cancelled">Cancelado</SelectItem>
-                </SelectContent>
-              </Select>
             </div>
             <Button size="medium" className="justify-center">
               Aplicar Filtro
             </Button>
           </div>
         </div>
-        <div className="w-full"></div>
+        <div className="grid w-full grid-cols-2 grid-rows-3 gap-4">
+          <ProductItem
+            imgSrc="picture-1.png"
+            productName="Sofá"
+            productPrice={1200.9}
+            productDescription="Sofá revestido em couro legítimo, com estrutura em madeira maciça e pés em metal cromado."
+            category="Móvel"
+            status="anunciado"
+          />
+          <ProductItem
+            imgSrc="picture-2.png"
+            productName="Camiseta masculina"
+            productPrice={35.89}
+            productDescription="Camiseta básica cinza, confeccionada em algodão 100%, com corte slim fit e gola redonda."
+            category="Vestuário"
+            status="anunciado"
+          />
+          <ProductItem
+            imgSrc="picture-3.png"
+            productName="Kit utensílios"
+            productPrice={86.79}
+            productDescription="Conjunto com 10 de cozinha, feitos medeira de bambu."
+            category="Utensílio"
+            status="anunciado"
+          />
+          <ProductItem
+            imgSrc="picture-4.png"
+            productName="Kit de cremes"
+            productPrice={159.9}
+            productDescription="Conjunto de cuidados com a pele contendo 3 cremes: hidratante facial, creme para as mãos e crememe anti-idade."
+            category="Saúde & Beleza"
+            status="anunciado"
+          />
+          <ProductItem
+            imgSrc="picture-5.png"
+            productName="Caderno de desenho"
+            productPrice={56}
+            productDescription="Caderno tamanho A4 com 120 páginas, gramatura de 180g/m², ideal para técnicas variadas como lápis, carvão e tinta."
+            category="Papelaria"
+            status="vendido"
+          />
+          <ProductItem
+            imgSrc="picture-6.png"
+            productName="Carro de brinquedo"
+            productPrice={24.6}
+            productDescription="Carrinho de brinquedo na cor amarela, feito de metal, com detalhes realistas."
+            category="Brinquedo"
+            status="desativado"
+          />
+        </div>
       </div>
     </div>
   )
