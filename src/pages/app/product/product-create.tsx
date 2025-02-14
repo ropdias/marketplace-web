560fdbd19a025596fb18bfacfec9b419458c38a7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-02-14 01:50:26 -0300

    add product-create and product-edit pages and additional routes

---
+++ src/pages/app/product/product-create.tsx
@@ -0,0 +1,21 @@
+import { Helmet } from 'react-helmet-async'
+
+import { getTailwindClass } from '@/lib/tailwindUtils'
+import { cn } from '@/lib/utils'
+
+export function ProductCreate() {
+  return (
+    <div className="m-auto flex w-full max-w-[66.875rem] flex-1 flex-col gap-10 px-5 pb-5">
+      <Helmet title="Cadastro de Produto" />
+      <div className="flex flex-col gap-2">
+        <p className={cn('text-gray-500', getTailwindClass('font-title-md'))}>
+          Novo produto
+        </p>
+        <p className={cn('text-gray-300', getTailwindClass('font-body-sm'))}>
+          Cadastre um produto para venda no marketplace
+        </p>
+      </div>
+      <div className="flex-start flex gap-6"></div>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard/dashboard'
+import { ProductCreate } from './pages/app/product/product-create'
+import { ProductEdit } from './pages/app/product/product-edit'
 import { Products } from './pages/app/products/products'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
@@ -13,6 +15,8 @@ export function AppRoutes() {
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Dashboard />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/product/create" element={<ProductCreate />} />
+        <Route path="/product/:id/edit" element={<ProductEdit />} />
       </Route>
       <Route path="/" element={<AuthLayout />}>
         <Route path="/sign-in" element={<SignIn />} />
