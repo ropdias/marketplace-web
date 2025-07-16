71237bd55e0fd61ba597c157d1501a5d36119501
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-15 22:03:00 -0300

    change MenuLink to use NavLink instead of Link

--- src/components/menu-link.tsx
+++ src/components/menu-link.tsx
@@ -1,26 +1,24 @@
-import { Link, LinkProps, useLocation } from 'react-router'
+import { NavLink, NavLinkProps } from 'react-router'
 
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
 
-export type MenuLinkProps = LinkProps
+export type MenuLinkProps = NavLinkProps
 
 export function MenuLink(props: MenuLinkProps) {
-  const { pathname } = useLocation()
-  const isActive = pathname === props.to
-
   return (
-    <Link
-      data-current={isActive}
-      className={cn(
-        'flex h-10 items-center justify-center gap-2 rounded-[10px] px-4 text-gray-300',
-        'hover:text-orange-base',
-        'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-base focus-visible:ring-offset-2',
-        isActive && 'bg-shape text-orange-base',
-        isActive
-          ? getTailwindClass('font-action-sm')
-          : getTailwindClass('font-body-sm'),
-      )}
+    <NavLink
+      className={({ isActive }) =>
+        cn(
+          'flex h-10 items-center justify-center gap-2 rounded-[10px] px-4 text-gray-300',
+          'hover:text-orange-base',
+          'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-base focus-visible:ring-offset-2',
+          isActive && 'bg-shape text-orange-base',
+          isActive
+            ? getTailwindClass('font-action-sm')
+            : getTailwindClass('font-body-sm'),
+        )
+      }
       {...props}
     />
   )
