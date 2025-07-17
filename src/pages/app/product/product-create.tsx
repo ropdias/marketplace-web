0d63cf85cbac78138d47bd363b5413c55c930c43
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-17 15:36:44 -0300

    add product create form basic layout

--- src/pages/app/product/product-create.tsx
+++ src/pages/app/product/product-create.tsx
@@ -1,5 +1,8 @@
 import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
 
@@ -18,7 +21,55 @@ export function ProductCreate() {
         </p>
       </div>
       <div className="flex-start flex gap-6">
-        <ProductImageUploader />
+        <div className="flex-shrink-0">
+          <ProductImageUploader />
+        </div>
+        <div className="flex w-full flex-col gap-8 rounded-[20px] bg-white p-8">
+          <p className={cn('text-gray-300', getTailwindClass('font-title-sm'))}>
+            Dados do produto
+          </p>
+          <form className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5">
+              <div className="flex gap-5">
+                <div className="w-full">
+                  <Input
+                    id="title"
+                    placeholder="Nome do produto"
+                    labelText="Título"
+                    isFilled={false}
+                  />
+                </div>
+                <div className="w-[12.5rem] flex-shrink-0">
+                  <Input
+                    id="value"
+                    placeholder="0,00"
+                    labelText="Valor"
+                    isFilled={false}
+                  />
+                </div>
+              </div>
+              <Input
+                id="description"
+                placeholder="Escreva detalhes sobre o produto, tamanho, características"
+                labelText="Descrição"
+                isFilled={false}
+              />
+              <div>Categoria aqui</div>
+            </div>
+            <div className="flex gap-3">
+              <Button
+                asChild
+                variant="outline"
+                className="h-12 flex-1 justify-center"
+              >
+                <Link to="/">Cancelar</Link>
+              </Button>
+              <Button className="h-12 flex-1 justify-center" type="submit">
+                Salvar e publicar
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )
