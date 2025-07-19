bbba9495de56d335ce35d880af9bbe02f62c2cc5
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-19 03:03:41 -0300

    make ProductForm use schema and utils from separated file

--- src/pages/app/product/product-form.tsx
+++ src/pages/app/product/product-form.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Controller, useForm } from 'react-hook-form'
 import { Link } from 'react-router'
-import { z } from 'zod'
 
 import { TagStatus } from '@/components/tag-status'
 import { Button } from '@/components/ui/button'
@@ -19,62 +18,9 @@ import { cn } from '@/lib/utils'
 import { Product } from '@/types/product'
 import { currencyApplyMask } from '@/utils/currency-apply-mask'
 
+import { productFormInputs, productFormSchema } from './product-form.schema'
+import { getProductFormDefaultValues } from './product-form.utils'
 import { ProductImageUploader } from './product-image-uploader'
-
-const productFormSchema = z.object({
-  productImage: z
-    .instanceof(File)
-    .refine((file) => file.type.startsWith('image/'), {
-      message: 'O arquivo precisa ser uma imagem',
-    })
-    .refine((file) => file.size < 5 * 1024 * 1024, {
-      message: 'A imagem deve ter menos de 5MB',
-    })
-    .optional(),
-  title: z.string().min(3, 'O título deve ter no mínimo 3 caracteres'),
-  price: z.string().refine((val) => val.trim() !== '', {
-    message: 'Informe um valor',
-  }),
-  description: z.string().min(3, 'A descrição deve ter no mínimo 3 caracteres'),
-  category: z
-    .string()
-    .refine(
-      (val) =>
-        [
-          'toy',
-          'furniture',
-          'stationery',
-          'healthAndBeauty',
-          'utensil',
-          'clothing',
-        ].includes(val),
-      {
-        message: 'Selecione uma opção válida',
-      },
-    ),
-})
-
-export type productFormInputs = z.infer<typeof productFormSchema>
-
-function getProductFormDefaultValues(product?: Product): productFormInputs {
-  if (!product) {
-    return {
-      title: '',
-      price: '',
-      description: '',
-      category: '',
-      productImage: undefined,
-    }
-  }
-
-  return {
-    title: product.title,
-    price: product.price,
-    description: product.description,
-    category: product.category,
-    productImage: undefined,
-  }
-}
 
 interface ProductFormProps {
   handleProductFormSubmit: (data: productFormInputs) => Promise<void>
