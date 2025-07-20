1336eb992e56f1620cc28abf2025860076a18b1e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-20 16:19:14 -0300

    update ProductItem and ProductItemProps based on Product interface

--- src/pages/app/products/product-item.tsx
+++ src/pages/app/products/product-item.tsx
@@ -4,34 +4,21 @@ import { TagCategory } from '@/components/tag-category'
 import { TagStatus } from '@/components/tag-status'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
+import { Product } from '@/types/product'
 
 interface ProductItemProps {
-  id: string
-  imgSrc: string
-  productName: string
-  productPrice: number
-  productDescription: string
-  category: string
-  status: 'anunciado' | 'vendido' | 'desativado'
+  product: Product
 }
 
-export function ProductItem({
-  id,
-  imgSrc,
-  productName,
-  productPrice,
-  productDescription,
-  status,
-  category,
-}: ProductItemProps) {
+export function ProductItem({ product }: ProductItemProps) {
   return (
     <Link
-      to={`/product/${id}/edit`}
+      to={`/product/${product.id}/edit`}
       className="relative flex w-full cursor-pointer flex-col gap-1 rounded-[20px] bg-white p-1 outline-none transition-shadow hover:ring-2 hover:ring-blue-base focus-visible:ring-2 focus-visible:ring-blue-base"
     >
       <img
-        src={imgSrc}
-        alt={`Imagem do ${productName}`}
+        src={product.attachments[0].url}
+        alt={`Imagem do ${product.title}`}
         className="h-full w-full rounded-[16px] object-cover"
       />
       <div className="flex flex-col gap-2 px-3 pb-4 pt-3">
@@ -39,7 +26,7 @@ export function ProductItem({
           <span
             className={cn('text-gray-400', getTailwindClass('font-subtitle'))}
           >
-            {productName}
+            {product.title}
           </span>
           <div className="flex items-baseline justify-center gap-1">
             <span
@@ -53,7 +40,7 @@ export function ProductItem({
               {new Intl.NumberFormat('pt-BR', {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
-              }).format(productPrice)}
+              }).format(product.priceInCents / 100)}
             </span>
           </div>
         </div>
@@ -63,12 +50,12 @@ export function ProductItem({
             getTailwindClass('font-body-sm'),
           )}
         >
-          {productDescription}
+          {product.description}
         </p>
       </div>
       <div className="absolute right-3 top-3 flex gap-1">
-        <TagStatus status={status} />
-        <TagCategory category={category} />
+        <TagStatus status={product.status} />
+        <TagCategory category={product.category.title} />
       </div>
     </Link>
   )
