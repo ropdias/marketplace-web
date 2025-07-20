22d5c5930a517f3c8d61e184a53162f570e6d69b
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-20 14:12:23 -0300

    chore: create Product interfaces following API response model

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,14 +1,76 @@
+export enum ProductStatus {
+  AVAILABLE = 'available',
+  SOLD = 'sold',
+  CANCELLED = 'cancelled',
+}
+
+export interface Avatar {
+  id: string
+  url: string
+}
+
+export interface Owner {
+  id: string
+  name: string
+  phone: string
+  email: string
+  avatar: Avatar | null
+}
+
+export interface Category {
+  id: string
+  title: string
+  slug: string
+}
+
+export interface Attachment {
+  id: string
+  url: string
+}
+
 export interface Product {
   id: string
   title: string
-  price: string
   description: string
-  category:
-    | 'toy'
-    | 'furniture'
-    | 'stationery'
-    | 'healthAndBeauty'
-    | 'utensil'
-    | 'clothing'
-  status: 'anunciado' | 'vendido' | 'desativado'
+  priceInCents: number
+  status: ProductStatus
+  owner: Owner
+  category: Category
+  attachments: Attachment[]
+}
+
+export interface CreateProductBody {
+  title: string
+  categoryId: string
+  description: string
+  priceInCents: number
+  attachmentsIds: string[]
+}
+
+export interface CreateProductResponse {
+  product: Product
+}
+
+export interface ListAllProductsResponse {
+  products: Product[]
+}
+
+export interface GetProductResponse {
+  product: Product
+}
+
+export interface EditProductRequestBody {
+  title: string
+  categoryId: string
+  description: string
+  priceInCents: number
+  attachmentsIds: string[]
+}
+
+export interface EditProductResponse {
+  product: Product
+}
+
+export interface ChangeProductStatusResponse {
+  product: Product
 }
