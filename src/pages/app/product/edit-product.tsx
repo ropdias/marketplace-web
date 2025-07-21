f76b86eafc4b59ad335c8b0b5e436807acb41fe8
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-21 00:23:10 -0300

    add FormLinks to edit page

--- src/pages/app/product/edit-product.tsx
+++ src/pages/app/product/edit-product.tsx
@@ -1,6 +1,8 @@
+import { ArrowLeft02Icon, Tick02Icon, UnavailableIcon } from 'hugeicons-react'
 import { Helmet } from 'react-helmet-async'
 import { useParams } from 'react-router'
 
+import { FormLink } from '@/components/form-link'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
 import { mockProducts } from '@/types/product'
@@ -21,14 +23,28 @@ export function EditProduct() {
   return (
     <>
       <Helmet title="Cadastro de Produto" />
-      <div className="flex flex-col gap-2">
-        <p className={cn('text-gray-500', getTailwindClass('font-title-md'))}>
-          Editar produto
-        </p>
-        <p className={cn('text-gray-300', getTailwindClass('font-body-sm'))}>
-          Gerencie as informações do produto cadastrado
-        </p>
+      <div className="flex items-end justify-between gap-6">
+        <div className="flex flex-col gap-2">
+          <FormLink to="/products" iconLeft={ArrowLeft02Icon}>
+            Voltar
+          </FormLink>
+          <p className={cn('text-gray-500', getTailwindClass('font-title-md'))}>
+            Editar produto
+          </p>
+          <p className={cn('text-gray-300', getTailwindClass('font-body-sm'))}>
+            Gerencie as informações do produto cadastrado
+          </p>
+        </div>
+        <div className="flex items-center gap-4 pr-3">
+          <FormLink to="/products" iconLeft={Tick02Icon}>
+            Marcar como vendido
+          </FormLink>
+          <FormLink to="/products" iconLeft={UnavailableIcon}>
+            Desativar anúncio
+          </FormLink>
+        </div>
       </div>
+
       <ProductForm
         handleProductFormSubmit={handleProductFormSubmit}
         action="edit"
