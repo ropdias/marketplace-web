a51fbf03001a26725dbbf6c1141743c678ebedf7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-23 03:35:27 -0300

    create filter form, apply RHF and update it based on searchParams

--- src/pages/app/products/products.tsx
+++ src/pages/app/products/products.tsx
@@ -1,6 +1,10 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { SaleTag02Icon, Search01Icon } from 'hugeicons-react'
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { Controller, useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -13,16 +17,61 @@ import {
 } from '@/components/ui/select'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
-import { mockProducts } from '@/types/product'
+import { mockProducts, ProductStatus } from '@/types/product'
 
 import { ProductItem } from './product-item'
 
-export function Products() {
-  const [selectedValue, setSelectedValue] = useState<string>('')
+const filterFormSchema = z.object({
+  search: z
+    .string()
+    .optional()
+    .transform((val) => val || undefined),
+  status: z
+    .union([z.nativeEnum(ProductStatus), z.literal('')])
+    .transform((val) => (val === '' ? undefined : val))
+    .optional(),
+})
 
-  const clearSelection = () => {
-    setSelectedValue('')
+type FilterFormInputs = z.infer<typeof filterFormSchema>
+
+const statusParamSchema = z.nativeEnum(ProductStatus)
+
+export function Products() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<FilterFormInputs>({
+    resolver: zodResolver(filterFormSchema),
+    defaultValues: {
+      search: '',
+      status: undefined,
+    },
+  })
+
+  async function handleFilterFormSubmit(data: FilterFormInputs) {
+    setSearchParams(() => {
+      const newParams = new URLSearchParams()
+
+      if (data.status) newParams.set('status', data.status)
+      if (data.search) newParams.set('search', data.search)
+
+      return newParams
+    })
   }
+
+  useEffect(() => {
+    const parsedStatus = statusParamSchema.safeParse(searchParams.get('status'))
+    const status = parsedStatus.success ? parsedStatus.data : undefined
+
+    const search = searchParams.get('search') ?? ''
+
+    reset({ status, search })
+  }, [searchParams, reset])
 
   return (
     <>
@@ -41,33 +90,51 @@ export function Products() {
           <p className={cn('text-gray-300', getTailwindClass('font-title-sm'))}>
             Filtrar
           </p>
-          <div className="flex w-full flex-col gap-10">
+          <form
+            className="flex w-full flex-col gap-10"
+            onSubmit={handleSubmit(handleFilterFormSubmit)}
+          >
             <div className="flex flex-col gap-5">
               <Input
                 id="search"
                 isFilled={false}
                 placeholder="Pesquisar"
                 iconLeft={Search01Icon}
+                {...register('search')}
               ></Input>
-              <Select value={selectedValue} onValueChange={setSelectedValue}>
-                <SelectTrigger
-                  iconLeft={SaleTag02Icon}
-                  onClear={clearSelection}
-                  selectedValue={selectedValue}
-                >
-                  <SelectValue placeholder="Status" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="announced">Anunciado</SelectItem>
-                  <SelectItem value="sold">Vendido</SelectItem>
-                  <SelectItem value="cancelled">Cancelado</SelectItem>
-                </SelectContent>
-              </Select>
+              <Controller
+                name="status"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    value={field.value ?? ''}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger
+                      onClear={() => field.onChange('')}
+                      iconLeft={SaleTag02Icon}
+                      selectedValue={field.value ?? ''}
+                    >
+                      <SelectValue placeholder="Status" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="available">Anunciado</SelectItem>
+                      <SelectItem value="sold">Vendido</SelectItem>
+                      <SelectItem value="cancelled">Cancelado</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
             </div>
-            <Button size="medium" className="justify-center">
+            <Button
+              size="medium"
+              className="justify-center"
+              disabled={isSubmitting}
+              type="submit"
+            >
               Aplicar Filtro
             </Button>
-          </div>
+          </form>
         </div>
         <div className="grid w-full grid-cols-2 grid-rows-3 gap-4">
           {mockProducts.map((product) => (
