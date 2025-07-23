a672aa645ba096617fb421db4c8420cce5373937
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-23 03:36:19 -0300

    create getAllProductsFromSeller api request

---
+++ src/api/products/get-all-products-from-seller.ts
@@ -0,0 +1,27 @@
+import { api } from '@/lib/axios'
+import { Product, ProductStatus } from '@/types/product'
+
+export interface GetAllProductsFromSellerQuery {
+  status?: ProductStatus | null
+  search?: string | null
+}
+
+export interface GetAllProductsFromSellerResponse {
+  products: Product[]
+}
+
+export async function getAllProductsFromSeller({
+  status,
+  search,
+}: GetAllProductsFromSellerQuery) {
+  const response = await api.get<GetAllProductsFromSellerResponse>(
+    '/products/me',
+    {
+      params: {
+        status,
+        search,
+      },
+    },
+  )
+  return response.data
+}
