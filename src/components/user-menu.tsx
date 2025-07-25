b954602dd3c88024360e717bf9092299f4b68c45
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-25 15:48:55 -0300

    add profile data and signOut to UserMenu

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -1,5 +1,9 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { Logout01Icon } from 'hugeicons-react'
+import { useNavigate } from 'react-router'
 
+import { getSellerProfile } from '@/api/sellers/get-seller-profile'
+import { signOut } from '@/api/sessions/sign-out'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
 
@@ -13,13 +17,28 @@ import {
 } from './ui/dropdown-menu'
 
 export function UserMenu() {
+  const navigate = useNavigate()
+
+  const { data: profile } = useQuery({
+    queryKey: ['seller-profile'],
+    queryFn: getSellerProfile,
+    staleTime: Infinity,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="h-12 w-12 rounded-[12px] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-base focus-visible:ring-offset-2">
         <img
-          src="/user-image-default.png"
+          src={profile?.seller.avatar?.url}
           alt="User Profile"
-          className="h-12 w-12 object-cover"
+          className="h-12 w-12 rounded-[12px] object-cover"
         />
       </DropdownMenuTrigger>
       <DropdownMenuContent
@@ -29,7 +48,7 @@ export function UserMenu() {
       >
         <DropdownMenuLabel className="flex items-center justify-center gap-3 p-0">
           <img
-            src="/user-image-default.png"
+            src={profile?.seller.avatar?.url}
             alt="User Profile"
             className="h-8 w-8 rounded-[12px] object-cover"
           />
@@ -39,11 +58,15 @@ export function UserMenu() {
               getTailwindClass('font-body-sm'),
             )}
           >
-            Brandon Ekstrom
+            {profile?.seller.name}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator className="bg-shape" />
-        <DropdownMenuItem className="flex items-center justify-between gap-2 p-0.5 text-orange-base [&_svg]:size-5">
+        <DropdownMenuItem
+          className="flex items-center justify-between gap-2 p-0.5 text-orange-base [&_svg]:size-5"
+          disabled={isSigningOut}
+          onClick={() => signOutFn()}
+        >
           <span className={cn(getTailwindClass('font-action-sm'))}>Sair</span>
           <Logout01Icon />
         </DropdownMenuItem>
