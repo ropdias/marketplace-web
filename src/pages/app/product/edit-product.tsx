868d121178f2da31a489e428abe45a740bda93a6
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-25 23:53:42 -0300

    get initial data from product by id using getProductById

--- src/pages/app/product/edit-product.tsx
+++ src/pages/app/product/edit-product.tsx
@@ -1,11 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
 import { ArrowLeft02Icon, Tick02Icon, UnavailableIcon } from 'hugeicons-react'
+import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
+import { toast } from 'sonner'
 
+import { getProductById } from '@/api/products/get-product-by-id'
 import { FormLink } from '@/components/form-link'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
-import { mockProducts } from '@/types/product'
 import { unmaskCurrencyToCents } from '@/utils/unmask-currency-to-cents'
 
 import { ProductForm } from './product-form'
@@ -13,16 +16,29 @@ import { productFormInputs } from './product-form.schema'
 
 export function EditProduct() {
   const { id } = useParams()
+  const navigate = useNavigate()
 
-  const product = mockProducts.find((product) => product.id === id)
+  const { data: products, isError } = useQuery({
+    queryKey: ['product', id],
+    queryFn: () => getProductById({ id: id! }),
+    enabled: !!id,
+    retry: false,
+  })
 
   async function handleProductFormSubmit(data: productFormInputs) {
     const priceInCents = unmaskCurrencyToCents(data.priceInCents)
   }
 
+  useEffect(() => {
+    if (isError) {
+      toast.error('Produto não encontrado.')
+      navigate('/products')
+    }
+  }, [isError, navigate])
+
   return (
     <>
-      <Helmet title="Cadastro de Produto" />
+      <Helmet title="Editar Produto" />
       <div className="flex items-end justify-between gap-6">
         <div className="flex flex-col gap-2">
           <FormLink to="/products" iconLeft={ArrowLeft02Icon}>
@@ -48,7 +64,7 @@ export function EditProduct() {
       <ProductForm
         handleProductFormSubmit={handleProductFormSubmit}
         action="edit"
-        initialData={product}
+        initialData={products?.product}
       />
     </>
   )
