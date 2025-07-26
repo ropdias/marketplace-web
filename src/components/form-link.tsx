59e6148f5745b8bd71eae8f847ff7aaf3fd1073e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-26 02:29:08 -0300

    change from Link to button

--- src/components/form-link.tsx
+++ src/components/form-link.tsx
@@ -1,9 +1,7 @@
-import { Link, LinkProps } from 'react-router'
-
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
 
-interface FormLinkProps extends LinkProps {
+interface FormLinkProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   iconLeft?: React.ElementType
   iconRight?: React.ElementType
 }
@@ -15,16 +13,18 @@ export function FormLink({
   ...props
 }: FormLinkProps) {
   return (
-    <Link
+    <button
+      type="button"
       {...props}
       className={cn(
         'flex items-center gap-2 whitespace-nowrap p-0.5 text-orange-base transition-colors hover:text-orange-dark focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-base focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
         getTailwindClass('font-action-sm'),
+        props.className,
       )}
     >
       {IconLeft && <IconLeft size={20} className={'flex-shrink-0'} />}
       {children}
       {IconRight && <IconRight size={20} className={'flex-shrink-0'} />}
-    </Link>
+    </button>
   )
 }
