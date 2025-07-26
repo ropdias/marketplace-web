522b3963d2a0c35ff3df9c0952e12d3e6718d782
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-26 02:29:40 -0300

    add logic to change product status

--- src/pages/app/product/edit-product.tsx
+++ src/pages/app/product/edit-product.tsx
@@ -1,20 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { ArrowLeft02Icon, Tick02Icon, UnavailableIcon } from 'hugeicons-react'
 import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useNavigate, useParams } from 'react-router'
 import { toast } from 'sonner'
 
+import { changeProductStatus } from '@/api/products/change-product-status'
 import { getProductById } from '@/api/products/get-product-by-id'
 import { FormLink } from '@/components/form-link'
 import { getTailwindClass } from '@/lib/tailwindUtils'
 import { cn } from '@/lib/utils'
+import { ProductStatus } from '@/types/product'
 
 import { ProductForm } from './product-form'
 
 export function EditProduct() {
   const { id } = useParams()
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
 
   const {
     data: getProductResponse,
@@ -25,6 +28,17 @@ export function EditProduct() {
     queryFn: () => getProductById({ id: id! }),
     enabled: !!id,
     retry: false,
+  })
+
+  const { mutateAsync: changeProductStatusFn } = useMutation({
+    mutationFn: changeProductStatus,
+    onSuccess: (updated) => {
+      queryClient.setQueryData(['product', updated.product.id], updated)
+      queryClient.invalidateQueries({
+        queryKey: ['products-from-seller'],
+        exact: false,
+      })
+    },
   })
 
   useEffect(() => {
@@ -41,7 +55,10 @@ export function EditProduct() {
       <Helmet title="Editar Produto" />
       <div className="flex items-end justify-between gap-6">
         <div className="flex flex-col gap-2">
-          <FormLink to="/products" iconLeft={ArrowLeft02Icon}>
+          <FormLink
+            iconLeft={ArrowLeft02Icon}
+            onClick={() => navigate('/products')}
+          >
             Voltar
           </FormLink>
           <p className={cn('text-gray-500', getTailwindClass('font-title-md'))}>
@@ -52,12 +69,68 @@ export function EditProduct() {
           </p>
         </div>
         <div className="flex items-center gap-4 pr-3">
-          <FormLink to="/products" iconLeft={Tick02Icon}>
-            Marcar como vendido
-          </FormLink>
-          <FormLink to="/products" iconLeft={UnavailableIcon}>
-            Desativar anúncio
-          </FormLink>
+          {getProductResponse?.product.status === ProductStatus.SOLD && (
+            <>
+              <FormLink
+                iconLeft={Tick02Icon}
+                onClick={() => {
+                  changeProductStatusFn({
+                    id: getProductResponse?.product.id,
+                    status: ProductStatus.AVAILABLE,
+                  })
+                }}
+              >
+                Marcar como disponível
+              </FormLink>
+              <FormLink iconLeft={UnavailableIcon} disabled>
+                Produto Vendido
+              </FormLink>
+            </>
+          )}
+          {getProductResponse?.product.status === ProductStatus.AVAILABLE && (
+            <>
+              <FormLink
+                iconLeft={Tick02Icon}
+                onClick={() => {
+                  changeProductStatusFn({
+                    id: getProductResponse?.product.id,
+                    status: ProductStatus.SOLD,
+                  })
+                }}
+              >
+                Marcar como vendido
+              </FormLink>
+              <FormLink
+                iconLeft={UnavailableIcon}
+                onClick={() => {
+                  changeProductStatusFn({
+                    id: getProductResponse?.product.id,
+                    status: ProductStatus.CANCELLED,
+                  })
+                }}
+              >
+                Desativar anúncio
+              </FormLink>
+            </>
+          )}
+          {getProductResponse?.product.status === ProductStatus.CANCELLED && (
+            <>
+              <FormLink iconLeft={Tick02Icon} disabled>
+                Produto Desabilitado
+              </FormLink>
+              <FormLink
+                iconLeft={UnavailableIcon}
+                onClick={() => {
+                  changeProductStatusFn({
+                    id: getProductResponse?.product.id,
+                    status: ProductStatus.AVAILABLE,
+                  })
+                }}
+              >
+                Reativar anúncio
+              </FormLink>
+            </>
+          )}
         </div>
       </div>
 
