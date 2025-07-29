7542d6c1ddcd15188a2f295bd4e9be414ecaf718
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-29 14:02:48 -0300

    use custom hook useProductsAvailableIn30Days

--- src/pages/app/dashboard/dashboard-products-available.tsx
+++ src/pages/app/dashboard/dashboard-products-available.tsx
@@ -1,34 +1,26 @@
-import { useQuery } from '@tanstack/react-query'
 import { Store04Icon } from 'hugeicons-react'
 import { useEffect } from 'react'
 import { toast } from 'sonner'
 
-import {
-  getProductsAvailableIn30Days,
-  mapGetProductsAvailableIn30DaysErrorMessage,
-} from '@/api/metrics/get-products-available-in-30-days'
+import { mapGetProductsAvailableIn30DaysErrorMessage } from '@/api/metrics/get-products-available-in-30-days'
+import { useProductsAvailableIn30Days } from '@/hooks/queries/use-products-available-in-30-days'
 
 import { DashboardItem } from './dashboard-item'
 
 export function DashboardProductsAvailable() {
   const {
     data: productsAvailableIn30Days,
-    error: productsAvailableIn30DaysError,
-    isError: productsAvailableIn30DaysIsError,
+    error,
+    isError,
     isLoading,
-  } = useQuery({
-    queryKey: ['products-available-in-30-days'],
-    queryFn: getProductsAvailableIn30Days,
-  })
+  } = useProductsAvailableIn30Days()
 
   useEffect(() => {
-    if (productsAvailableIn30DaysIsError && productsAvailableIn30DaysError) {
-      const message = mapGetProductsAvailableIn30DaysErrorMessage(
-        productsAvailableIn30DaysError,
-      )
+    if (isError && error) {
+      const message = mapGetProductsAvailableIn30DaysErrorMessage(error)
       toast.error(message)
     }
-  }, [productsAvailableIn30DaysError, productsAvailableIn30DaysIsError])
+  }, [error, isError])
 
   return (
     <DashboardItem
