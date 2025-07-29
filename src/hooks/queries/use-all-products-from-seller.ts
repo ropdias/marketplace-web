bec77fbb47e96dc9758bb6fdbad2ccb20aeca55c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-29 14:20:02 -0300

    create useAllProductsFromSeller

---
+++ src/hooks/queries/use-all-products-from-seller.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getAllProductsFromSeller } from '@/api/products/get-all-products-from-seller'
+import { ProductStatus } from '@/types/product'
+
+interface UseAllProductsFromSellerProps {
+  status?: ProductStatus
+  search?: string
+}
+
+export function useAllProductsFromSeller({
+  status,
+  search,
+}: UseAllProductsFromSellerProps) {
+  return useQuery({
+    queryKey: ['products-from-seller', status, search],
+    queryFn: () =>
+      getAllProductsFromSeller({
+        status,
+        search,
+      }),
+  })
+}
