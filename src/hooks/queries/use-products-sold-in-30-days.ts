ca89e6074052d2256ae9cece95ec763af9561826
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-07-29 14:04:56 -0300

    create useProductsSoldIn30Days

---
+++ src/hooks/queries/use-products-sold-in-30-days.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getProductsSoldIn30Days } from '@/api/metrics/get-products-sold-in-30-days'
+
+export function useProductsSoldIn30Days() {
+  return useQuery({
+    queryKey: ['products-sold-in-30-days'],
+    queryFn: getProductsSoldIn30Days,
+  })
+}
