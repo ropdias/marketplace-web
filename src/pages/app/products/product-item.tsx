2e2c65ff54353f96a351c715277a6f6f535d0f61
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-12-03 15:49:52 -0300

    fix: add gap to product item title container for better spacing

--- src/pages/app/products/product-item.tsx
+++ src/pages/app/products/product-item.tsx
@@ -29,7 +29,7 @@ export function ProductItem({ product }: ProductItemProps) {
         </div>
       )}
       <div className="flex flex-col gap-2 px-3 pb-4 pt-3">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-2">
           <span
             className={cn('text-gray-400', getTailwindClass('font-subtitle'))}
           >
